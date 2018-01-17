79008c3619cd3a3564b49124377b93d6259452e3
Lion <815694355@qq.com>
2018-01-17 19:45:55 +0800

    适配调整

--- lib/carrierwave/encrypter_decrypter/openssl/aes.rb
+++ lib/carrierwave/encrypter_decrypter/openssl/aes.rb
@@ -1,30 +1,30 @@
 module Openssl
   module Aes
     def self.encrypt_for(obj)
-      begin
+      begin 
         model = obj.model
         mounted_as = obj.mounted_as
-        cipher = OpenSSL::Cipher.new("AES-#{Carrierwave::EncrypterDecrypter.configuration.key_size}-CBC")
+        cipher = OpenSSL::Cipher::AES.new(Carrierwave::EncrypterDecrypter.configuration.key_size, :CBC)
         cipher.encrypt
         iv = cipher.random_iv
         model.iv = iv
         key = cipher.random_key
         model.key = key
         model.save!
-
+        
         original_file_path = File.expand_path(obj.store_path, obj.root)
         encrypted_file_path = File.expand_path(obj.store_path, obj.root) + ".enc"
         buf = ""
         File.open(encrypted_file_path, "wb") do |outf|
-          File.open(model.send(mounted_as).path, "rb") do |inf|
+          File.open(original_file_path, "rb") do |inf|
             while inf.read(4096, buf)
               outf << cipher.update(buf)
             end
             outf << cipher.final
           end
         end
-        File.unlink(model.send(mounted_as).path)
-      rescue Exception => e
+        File.unlink(original_file_path)     
+      rescue Exception => e  
         puts "****************************#{e.message}"
         puts "****************************#{e.backtrace.inspect}"
       end
@@ -34,14 +34,15 @@ module Openssl
       begin
         model = obj
         mounted_as = opts[:mounted_as]
-        cipher = OpenSSL::Cipher.new("AES-#{Carrierwave::EncrypterDecrypter.configuration.key_size}-CBC")
+        cipher = OpenSSL::Cipher::AES.new(Carrierwave::EncrypterDecrypter.configuration.key_size, :CBC)
         cipher.decrypt
-        cipher.iv = model.iv
+        cipher.iv = model.iv 
         cipher.key = model.key
         buf = ""
-
-        original_file_path =  obj.send(mounted_as).root + obj.send(mounted_as).url
-        encrypted_file_path =  obj.send(mounted_as).root + obj.send(mounted_as).url  + ".enc"
+        
+        original_file_path = Tempfile.new(obj.attributes[mounted_as.to_s]).path
+        # original_file_path =  obj.send(mounted_as).path 
+        encrypted_file_path =  obj.send(mounted_as).path  + ".enc"
 
         File.open(original_file_path, "wb") do |outf|
           File.open(encrypted_file_path, "rb") do |inf|
@@ -51,6 +52,7 @@ module Openssl
             outf << cipher.final
           end
         end
+        original_file_path
       rescue Exception => e
         puts "****************************#{e.message}"
         puts "****************************#{e.backtrace.inspect}"
